24a5c91fd05ba6e53c07633164f645297f285bde
/taianeterassaka <taianeterassaka@usp.br>
2024-11-11 14:44:13 -0300

    aviao.java

---
+++ aviao.java
@@ -0,0 +1,83 @@
+public class aviao{
+    private int id;
+    private String modelo;
+    private int ano;
+    private int capacidade_passageiros;
+    private int ativo;  //1=> ativo 0=>inativo
+    
+    //métodos construtores
+    public aviao(int id, String modelo, int ano, int capacidade_passageiros){
+        this.id = id;
+        this.modelo = modelo;
+        this.ano = ano ;
+        this.capacidade_passageiros = capacidade_passageiros;
+        this.ativo = 1;
+    }
+    
+    //metodos setters
+    public void setAtivo(int ativo, boolean permissao){
+        if(permissao = true){
+            this.ativo= ativo;
+        }else{
+           System.out.println("Usuário sem permissão para realizar operação");
+        }
+    }
+    
+
+    public void setId(int id, boolean permissao){
+        if(permissao = true){
+            this.id = id;
+        }else{
+           System.out.println("Usuário sem permissão para realizar operação");
+        }
+    }
+    
+    public void setModelo(String modelo, boolean permissao){
+        if(permissao = true){
+            this.modelo = modelo;
+        }else{
+            System.out.println("Usuário sem permissão para realizar operação");
+        }
+    }
+    
+    public void setAno(int ano, boolean permissao){
+        if(permissao = true){
+            this.ano = ano;
+        }else{
+            System.out.println("Usuário sem permissão para realizar operação");
+        }
+    }
+    
+    public void getCapacidade(int capacidade_passageiros, boolean permissao){
+        if(permissao = true){
+           this.capacidade_passageiros = capacidade_passageiros;
+        }else{
+           System.out.println("Usuário sem permissão para realizar operação");
+        }
+    }
+    
+    //métodos getters
+    public int getId(){
+        return id;
+    }
+    
+    public String getModelo(){
+        return modelo;
+    }
+    
+    public int getAno(){
+        return ano;
+    }
+    
+    public int getAtivo(){
+        return ativo;
+    }
+    
+    public int getCapacidade(){
+        return capacidade_passageiros;
+    }
+    
+    //metodos abstratos
+    
+    
+}
